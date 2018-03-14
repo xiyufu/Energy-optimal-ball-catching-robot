fea92093f514ec1253447d15aa9d019d53372b79
Xiyu Fu <xiyu_fu@outlook.com>
2018-03-15 00:04:20 +0100

    Update arm_plot.m

--- arm_plot.m
+++ arm_plot.m
@@ -4,18 +4,14 @@ if flag_integratorF == 1
 else
     kF = k;
 end
-sc1 = scatter(t(1:kF), x(1,1:kF));
-sc1.Marker = 'x';
-sc2 = scatter(t(1:kF), x(2,1:kF));
-sc2.Marker = 'x';
-sc3 = scatter(t(1:kF), x(3,1:kF));
-sc3.Marker = 'x';
-sc4 = scatter(t(1:kF), x(4,1:kF));
-sc4.Marker = 'x';
-sc5 = scatter(t(1:kF), x(5,1:kF));
-sc5.Marker = 'x';
-sc6 = scatter(t(1:kF), x(6,1:kF));
-sc6.Marker = 'x';
-
+for i = 1:6
+    plot(t(1:kF), x(i, 1:kF), 'x');
+end
 plot(t(1:k), q_ref(:,1:k));
 plot(t(1:kF), q_itc(1)*ones(1,kF),t(1:kF), q_itc(2)*ones(1,kF));
+
+figure; hold
+for i = 7:12
+    plot(t(1:kF), x(i,1:kF), '--');
+end
+plot(t(1:k), dq_ref(:,1:k));

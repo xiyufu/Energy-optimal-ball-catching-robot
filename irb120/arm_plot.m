6ad3d8bd5a4deaf1865ba5349479a493824c8a38
Xiyu Fu <xiyu.fu@student.kuleuven.be>
2018-03-17 18:30:30 +0100

    Files classified

---
+++ irb120/arm_plot.m
@@ -0,0 +1,17 @@
+figure; hold
+if flag_integratorF == 1
+    kF = k+ii;
+else
+    kF = k;
+end
+for i = 1:6
+    plot(t(1:kF), x(i, 1:kF), 'x');
+end
+plot(t(1:k), q_ref(:,1:k));
+plot(t(1:kF), q_itc(1)*ones(1,kF),t(1:kF), q_itc(2)*ones(1,kF));
+
+figure; hold
+for i = 7:12
+    plot(t(1:kF), x(i,1:kF), '--');
+end
+plot(t(1:k), dq_ref(:,1:k));
